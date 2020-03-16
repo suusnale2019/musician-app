43a93dc02b295b73c8eb18c1e03e785a5532f6de
James Clark <57755656+suusnale2019@users.noreply.github.com>
2020-03-16 14:01:25 +0530

    Update musician.js
    Edited code

--- routes/musician.js
+++ routes/musician.js
@@ -6,7 +6,7 @@ const schema = require('../store/schema');
 
 // healthcheck
 router.get('/health', (req, res) => {
-  res.status('200').send("Status: It looks okey...!");
+  res.status('200').send("Status: okey!");
 });
 
 // retrieve all musicians from data store
