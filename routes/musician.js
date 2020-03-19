f9c0346ed1e6ccbd1a8fe477af685b789f3c7559
James Clark <57755656+suusnale2019@users.noreply.github.com>
2020-03-19 19:36:46 +0530

    Update musician.js
    code option 2

--- routes/musician.js
+++ routes/musician.js
@@ -9,6 +9,11 @@ router.get('/health', (req, res) => {
   res.status('200').send("Status: okey!");
 });
 
+// healthcheck
+router.get('/health1', (req, res) => {
+  res.status('200').send("Status2: All is okey!");
+});
+
 // retrieve all musicians from data store
 router.get('/all', (req, res) => {
   const { musician } = req.app.locals;
